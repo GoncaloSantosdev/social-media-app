8511d98c730a4954d10678b673321e7a157c9ba0
GoncaloSantosdev <goncalosantosprofissional@gmail.com>
2023-09-01 20:46:43 +0100

    clerk working

---
+++ src/app/(auth)/onboarding/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const OnBoarding = () => {
+  return (
+    <main>
+      <h1 className="head-text">Onboarding</h1>
+    </main>
+  );
+};
+
+export default OnBoarding;
---
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,7 @@
+import { SignUp } from "@clerk/nextjs";
+
+const SignUpPage = async () => {
+  return <SignUp />;
+};
+
+export default SignUpPage;
