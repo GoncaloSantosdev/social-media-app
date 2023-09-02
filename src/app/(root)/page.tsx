769ec87e657a0359b1fa82864af779045a224332
GoncaloSantosdev <goncalosantosprofissional@gmail.com>
2023-09-02 17:20:17 +0100

    layout done

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 const HomeScreen = () => {
   return (
     <div>
-      <UserButton afterSignOutUrl="/" />
+      <h1 className="head-text text-left">Home</h1>
     </div>
   );
 };
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as TopBar } from "./shared/TobBar";
+export { default as LeftSideBar } from "./shared/LeftSideBar";
+export { default as RightSideBar } from "./shared/RightSideBar";
+export { default as BottomBar } from "./shared/BottomBar";
